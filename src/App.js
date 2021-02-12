e8d6509dc6786a6d2932b4c99e85b7d428bd646d
Ghofrane Ben Hmaid <ghofranebh90@gmail.com>
2021-02-13 00:30:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-
 import './App.scss';
 import Cursor from './Cursor';
 
 function App() {
-  return (
-    <div className="container">
-      <div className='wrapper nav-links'>
-<Cursor/>
-      {/* <div className='inner-header'></div> */}
+   return (
+      <div className='inner'>
+         <div className='text' >
+            <h1 className='text__title'>
+               Show name of shapes  on hover over.
+            </h1>
+         </div>
+         <div className='container'>
+            <Cursor />
+            <div className='square mr'></div>
+            <div className='cercle mr'></div>
+            <div className='triangle mr'></div>
+            <div className='star mr'></div>
+            <div className='heart mr'></div>
+            <div className='rectangle mr'></div>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default App;
--- src/Cursor.js
+++ src/Cursor.js
@@ -2,38 +2,83 @@ import React, { useRef, useEffect } from 'react';
 
 import './Cursor.scss';
 
-const Cursor = () =>
-{
-    const cursorRef = useRef();
+const Cursor = () => {
+   const cursorRef = useRef();
 
-     useEffect(() => {
+   useEffect(() => {
       document.addEventListener('mousemove', (e) => {
          const { clientX, clientY } = e;
          const mouseX = clientX - cursorRef.current.clientWidth / 2;
          const mouseY = clientY - cursorRef.current.clientHeight / 2;
 
-         // cursorRef.current.style.transform = `translate3d(${ mouseX }px, ${ mouseY }px, 0)`;
          cursorRef.current.setAttribute(
             'style',
             `top:${mouseY}px; left:${mouseX}px;`
          );
       });
-     }, []);
-    
-     useEffect(() => {
-      let navLinks = document.querySelectorAll('.nav-links');
-      navLinks.forEach((link) => {
+   }, []);
+
+   useEffect(() =>
+   {
+
+      let Square = document.querySelectorAll('.square');
+      let Rectangle = document.querySelectorAll('.rectangle');
+      let Cercle = document.querySelectorAll('.cercle');
+      let Triangle = document.querySelectorAll('.triangle');
+      let Star = document.querySelectorAll('.star');
+      let Heart = document.querySelectorAll('.heart');
+
+      Square.forEach((link) => {
          link.addEventListener('mouseleave', () => {
-            cursorRef.current.classList.remove('link-grow');
-            
+            cursorRef.current.classList.remove('link-square');
          });
          link.addEventListener('mouseover', () => {
-            cursorRef.current.classList.add('link-grow');
+            cursorRef.current.classList.add('link-square');
          });
       });
-     }, []);
-    
-   return <div className='cursor' ref={cursorRef}></div>;
-}
+      Rectangle.forEach((link) => {
+         link.addEventListener('mouseleave', () => {
+            cursorRef.current.classList.remove('link-rectangle');
+         });
+         link.addEventListener('mouseover', () => {
+            cursorRef.current.classList.add('link-rectangle');
+         });
+      });
+      Cercle.forEach((link) => {
+         link.addEventListener('mouseleave', () => {
+            cursorRef.current.classList.remove('link-cercle');
+         });
+         link.addEventListener('mouseover', () => {
+            cursorRef.current.classList.add('link-cercle');
+         });
+      });
+      Triangle.forEach((link) => {
+         link.addEventListener('mouseleave', () => {
+            cursorRef.current.classList.remove('link-triangle');
+         });
+         link.addEventListener('mouseover', () => {
+            cursorRef.current.classList.add('link-triangle');
+         });
+      });
+      Star.forEach((link) => {
+         link.addEventListener('mouseleave', () => {
+            cursorRef.current.classList.remove('link-star');
+         });
+         link.addEventListener('mouseover', () => {
+            cursorRef.current.classList.add('link-star');
+         });
+      });
+      Heart.forEach((link) => {
+         link.addEventListener('mouseleave', () => {
+            cursorRef.current.classList.remove('link-heart');
+         });
+         link.addEventListener('mouseover', () => {
+            cursorRef.current.classList.add('link-heart');
+         });
+      });
+   }, []);
 
-export default Cursor
+   return <div className='cursor' ref={cursorRef}></div>;
+};
+
+export default Cursor;
